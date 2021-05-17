9546bb0f1e07b6d67a6f8d4de2fb05f7b89dfb80
devgithubs <arthur.oleary24@mail.dcu.ie>
2021-05-17 22:53:52 +0000

    add additional features. extra data in json file

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -17,6 +18,17 @@ def about():
     return render_template("about.html", page_title="About", company = data)
 
 
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {} 
+    with open("data/company.json","r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
+
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html",page_title="Contact")
@@ -25,6 +37,7 @@ def contact():
 @app.route("/careers")
 def careers():
     return render_template("careers.html",page_title="Careers")
+
 
 if __name__ == "__main__":
     app.run(
