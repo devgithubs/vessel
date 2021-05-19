9b61fafc1cc19841fcaa81f9aa55c109651afefb
devgithubs <arthur.oleary24@mail.dcu.ie>
2021-05-19 01:27:41 +0000

    functionality for contact page. flash messages

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
+
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -13,7 +17,7 @@ def index():
 @app.route("/about")
 def about():
     data = []
-    with open("data/company.json","r") as json_data:
+    with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company = data)
 
@@ -21,7 +25,7 @@ def about():
 @app.route("/about/<member_name>")
 def about_member(member_name):
     member = {} 
-    with open("data/company.json","r") as json_data:
+    with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
         for obj in data:
             if obj["url"] == member_name:
@@ -29,18 +33,21 @@ def about_member(member_name):
     return render_template("member.html", member=member)
 
 
-@app.route("/contact")
+@app.route("/contact", methods = ["GET", "POST"])
 def contact():
-    return render_template("contact.html",page_title="Contact")
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html",page_title="Careers")
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP","0.0.0.0"),
-        port=int(os.environ.get("PORT","5000")),
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
         debug=True)
